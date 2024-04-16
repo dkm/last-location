2f278d4300cc2bc1cb676c7df7ddd217527cfae2
Marc Poulhiès <dkm@kataplop.net>
2024-04-16 08:19:29 +0200

    tests: initial tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,16 +1,74 @@
 #[cfg(test)]
 use super::*;
-use axum::{
-    body::Body,
-    extract::connect_info::MockConnectInfo,
-    http::{self, Request, StatusCode},
-};
-//use http_body_util::BodyExt; // for `collect`
-use serde_json::{json, Value};
-use tokio::net::TcpListener;
-use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
+use ::axum_test::TestServer;
+use last_position::{create_user, get_user};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct TestForm {
+    pub user_id: i32,
+    pub device_timestamp: i32,
+    pub lat: f64,
+    pub lon: f64,
+}
 
 #[tokio::test]
-async fn simple_test() {
-    let app = app();
+async fn simple_location_post_get() {
+    let db_url = "simple_location_post_get.sqlite";
+    let manager = Manager::new(db_url, deadpool_diesel::Runtime::Tokio1);
+    let pool = deadpool_diesel::sqlite::Pool::builder(manager)
+        .build()
+        .unwrap();
+
+    {
+        let conn = pool.get().await.unwrap();
+        conn.interact(|conn| conn.run_pending_migrations(MIGRATIONS).map(|_| ()))
+            .await
+            .unwrap()
+            .unwrap();
+    }
+
+    {
+        let conn = pool.get().await.unwrap();
+        conn.interact(|conn| create_user(conn, "sample_user"))
+            .await
+            .unwrap()
+            .unwrap();
+    }
+
+    {
+        let conn = pool.get().await.unwrap();
+        conn.interact(|conn| get_user(conn, 1i32))
+            .await
+            .unwrap()
+            .unwrap();
+    }
+
+    let app = app(pool).await;
+
+    let server = TestServer::new(app).unwrap();
+
+    let response = server
+        .post(&"/api/set_last_location")
+        .form(&TestForm {
+            user_id: 1i32,
+            device_timestamp: 1i32,
+            lat: 32.0f64,
+            lon: 22.0f64,
+        })
+        .await;
+    response.assert_status_ok();
+
+    let response = server
+        .get("/api/get_last_location")
+        .add_query_param("uid", "1")
+        .await;
+    response.assert_status_ok();
+
+    let json_res = response.json::<UserLocationPoint>();
+
+    assert_eq!(json_res.user_id, 1);
+    assert_eq!(json_res.device_timestamp, 1);
+    assert_eq!(json_res.lat, 32.0f64);
+    assert_eq!(json_res.lon, 22.0f64);
 }
